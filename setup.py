b05a0ab7ca2071d05ea0e91d890e10638bcf3340
shekhar2k1 <shekhar2k1@gmail.com>
2016-09-25 07:17:10 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='piglatin',
     description='PigLatin Translation app',
-    url='https://github.com',
+    url='https://github.com/shekhar2k1/PigLatin',
     version='0.0.1',
     author_email='abc@gmail.com',
     packages=['piglatin', ],
